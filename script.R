3af5512c4ca06d36dc6e2f17dec16b57feb5ebe2
Kevin Siswandi <kevins@Kevins-MacBook-Air-2.local>
2016-03-25 20:58:35 +0800

    Do not remove variables upon OHE and do not replace NA but just create new fields.

--- script.R
+++ script.R
@@ -8,8 +8,8 @@ library(dplyr)
 
 # dat_train <- read.csv("../input/train.csv", stringsAsFactors = F)
 # dat_test <- read.csv("../input/test.csv", stringsAsFactors = F)
-dat_train <- read.csv("C:/Users/jingpingwong/Downloads/santander/train.csv", stringsAsFactors = FALSE)
-dat_test <- read.csv("C:/Users/jingpingwong/Downloads/santander/test.csv", stringsAsFactors = FALSE)
+dat_train <- read.csv("~/santander2/train.csv", stringsAsFactors = FALSE)
+dat_test <- read.csv("~/santander2/test.csv", stringsAsFactors = FALSE)
 
 dat_test$TARGET <- NA
 
@@ -21,9 +21,6 @@ all_dat <- rbind(dat_train, dat_test)
 #################################################################
 ####################   Feature engineering ######################
 #################################################################
-
-#Creating a sum-of-zero field
-all_dat$n0 <- apply(all_dat, 1, function(x)(sum(x == 0, na.rm=T)))
 
 # Removing the constant variables
 for (i in names(all_dat)[-1])
@@ -40,26 +37,26 @@ for (i in names(all_dat)[-1])
 all_dat <- all_dat[!duplicated(lapply(all_dat, summary))]
 
 #Removing highly correlated variables
-cor_v <- abs(cor(all_dat))
-diag(cor_v) <- 0
-cor_v[upper.tri(cor_v)] <- 0
-cor_v <- as.data.frame(which(cor_v > 0.85, arr.ind = T))
-cat("Removing ", names(all_dat)[-unique(cor_v$row)])
-all_dat <- all_dat[,-unique(cor_v$row)]
+# cor_v <- abs(cor(all_dat))
+# diag(cor_v) <- 0
+# cor_v[upper.tri(cor_v)] <- 0
+# cor_v <- as.data.frame(which(cor_v > 0.85, arr.ind = T))
+# cat("Removing ", names(all_dat)[-unique(cor_v$row)])
+# all_dat <- all_dat[,-unique(cor_v$row)]
 
 #Handle missing values
-all_dat$var3[all_dat$var3 == -999999] <- NA
+all_dat$var3NA <- ifelse(all_dat$var3 == -999999, 1, 0)
 
 #Renaming known variables
 all_dat <- rename(all_dat, age=var15)
 
-#Creating variable age of type categorical
-# all_dat$age <- as.factor(round(all_dat$age/10, 0))
+#Creating variable of type categorical
+all_dat$ageDiscrete <- as.factor(round(all_dat$age/10, 0))
 
 #One-hot encoding
-# dummies <- dummyVars(~ age, data=all_dat)
-# ohe <- as.data.frame(predict(dummies, newdata=all_dat))
-# all_dat <- cbind(all_dat[, ! names(all_dat) %in% c('age')], ohe)
+dummies <- dummyVars(~ ageDiscrete, data=all_dat)
+ohe <- as.data.frame(predict(dummies, newdata=all_dat))
+all_dat <- cbind(all_dat[, ! names(all_dat) %in% c('ageDiscrete')], ohe)
 
 ###############################################################################
 ############################## Model Training #################################
@@ -71,19 +68,35 @@ test <- all_dat[-(1:nrow(dat_train)), ]
 
 
 #Building the model
-param <- list("objective" = "binary:logistic",booster = "gbtree", gamma=1,
-			  "eval_metric" = "auc", nthread=2, colsample_bytree=0.9, subsample=0.9, min_child_weight=1)
+param <- list(objective = "binary:logistic",
+              booster = "gbtree",
+			        eval_metric = "auc",
+              nthread=2,
+			        eta=0.02,
+			        max_depth=5,
+			        colsample_bytree=0.7,
+			        subsample=0.7)
 
 #Parameter values are obtained from cross-validation
-set.seed(88)
-xgbcv <- xgb.cv(data = as.matrix(train[, !names(train) %in% c("ID", "TARGET")]), params = param,
-                                label=train$TARGET, nrounds = 1500, max.depth = 7, eta = 0.03, maximize = F, missing=NA, nfold=5)
-set.seed(88)
-xgbmodel <- xgboost(data = as.matrix(train[, !names(train) %in% c("ID", "TARGET")]), params = param,
-                    label=train$TARGET, nrounds = 100, max.depth = 5, eta = 0.001, maximize = T, missing=NA)
+#445
+xgbcv <- xgb.cv(data = as.matrix(train[, !names(train) %in% c("ID", "TARGET")]),
+                nrounds = 550,
+                label=train$TARGET,
+                nfold=5,
+                params = param,
+                verbose = 2,
+                maximize=FALSE)
+                
+xgbmodel <- xgboost(data = as.matrix(train[, !names(train) %in% c("ID", "TARGET")]),
+                    label=train$TARGET,
+                    nrounds = 550,
+                    params = param,
+                    verbose=2,
+                    maximize = F
+                    )
 
 #Prediction
-preds <- predict(xgbmodel, newdata = data.matrix(test[, ! names(test) %in% c("ID", "TARGET")]), missing=NA)
+preds <- predict(xgbmodel, newdata = data.matrix(test[, ! names(test) %in% c("ID", "TARGET")]))
 submission <- data.frame(ID = test$ID, TARGET = preds)
 
 write.csv(submission, "submission.csv", row.names = FALSE)
