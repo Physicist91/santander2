87b4b5af45715726cc8f08cdd5a5d9128e9be834
Kevin Siswandi <kevins@Kevins-MacBook-Air-2.local>
2016-04-25 22:14:05 +0800

    Starting rf model

--- rf.R
+++ rf.R
@@ -1,31 +1,15 @@
 library("randomForest")
-library("ROSE")
 
 source("feature.R")
 
 # splitting the data for model
-train <- all_dat[all_dat$ID %in% dat_train$ID, ]
-test <- all_dat[all_dat$ID %in% dat_test$ID, ]
+training <- all_dat[all_dat$ID %in% dat_train$ID, ]
+testing <- all_dat[all_dat$ID %in% dat_test$ID, ]
 
-# generating synthetic data
-train <- ROSE(TARGET ~ ., data=train[!names(train) == 'ID'], seed=8888)$data
+training$ID <- NULL
+training$TARGET <- as.factor(training$TARGET)
 
-#Tuning
-mtry.max = ncol(train) - 1
-err.rf <- rep(0, mtry.max)
-for(m in 1:mtry.max){
-  set.seed(1234)
-  
-  rfmodel <- randomForest(as.factor(TARGET) ~ ., data=train[, !names(train) == 'ID'], mtry = m, ntree= 501, na.action=na.omit)
-  
-  err.rf[m] <- rf$err.rate[501]
-}
-
-plot(1:mtry.max, err.rf, type = "b", xlab = "mtry", ylab = "OOB error")
-
-best.mtry <- which.min(err.rf)
-
-rfmodel <- randomForest(as.factor(TARGET) ~ ., data=train[, !names(train) == 'ID'], ntree= 501, na.action=na.omit)
+rfmodel <- randomForest(TARGET ~ ., data=training, ntree= 101, do.trace=TRUE, mtry=145)
 
 preds <- predict(rfmodel, newdata=test[, !names(test) %in% c("ID", "TARGET")], type='vote')
 preds
